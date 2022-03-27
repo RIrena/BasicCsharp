ff97740f6cdf1a498fe3c061e9c70c01d3150932
RIrena <irenaristeska@hotmail.com>
2022-03-27 21:44:05 +0200

    Homework from Class 02

---
+++ Homeworks from Class 02/Exercise6FromClass02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6FromClass02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise 06 - from Class 2");
+
+            //Create two variables and initialize  them with a keyboard input
+            int number1;
+            int number2;
+            Console.WriteLine("Please enter first number ");
+            bool ifSuccessfull = int.TryParse(Console.ReadLine(), out number1);
+            Console.WriteLine("Please enter second number");
+            bool ifSuccessFull = int.TryParse(Console.ReadLine(), out number2);
+
+
+            // Write code that can test which number is larger
+            if (number1 > number2)
+            {
+                Console.WriteLine("First number is larger then second");
+            }
+            else if (number2 > number1)
+            {
+                Console.WriteLine("Second number is larger then first");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are same");
+            }
+
+            // Write code that can test the numbers whether they are even or odd
+            if (number1 % 2 == 0)
+            {
+                Console.WriteLine("First 1 is EVEN");
+            }
+            else
+            {
+                Console.WriteLine("First 1 is ODD");
+            }
+            if (number2 % 2 == 0)
+            {
+                Console.WriteLine("Second 2 is EVEN");
+            }
+            else
+            {
+                Console.WriteLine("Second 2 is ODD");
+            }
+
+            // Write the larger number from the two in the console
+            if (number1 > number2)
+            {
+                Console.WriteLine(number1);
+            }
+            else
+            {
+                Console.WriteLine(number2);
+
+            }
+
+            //After that write if the number is even or odd
+            if (number1 % 2 == 0)
+            {
+                Console.WriteLine("Larger number is EVEN");
+            }
+            else if (number2 % 2 == 0)
+            {
+                Console.WriteLine("Larger number is EVEN");
+            }
+            else
+            {
+                Console.WriteLine("Larger number is ODD");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homeworks from Class 02/Homework1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homeworks from Class 02");
+            #region Homework1
+
+            Console.WriteLine("---------- REAL CALCULATOR ----------");
+
+            int inputFirstNumber = 0;
+            int inputSecondNumber = 0;
+            int Calculation;
+
+            Console.WriteLine("Enter the First Number");
+            inputFirstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            inputSecondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operation:");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine("The result is:" + (inputFirstNumber + inputSecondNumber));
+                    break;
+                case "-":
+                    Console.WriteLine("The result is:" + (inputFirstNumber - inputSecondNumber));
+                    break;
+                case "*":
+                    Console.WriteLine("The result is:" + (inputFirstNumber * inputSecondNumber));
+                    break;
+                case "/":
+                    Console.WriteLine("The result is:" + (inputFirstNumber / inputSecondNumber));
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    break;
+            }
+
+            #endregion
+
+            #region Homework2
+            Console.WriteLine("----------- AVERAGE NUMBER ----------- ");
+
+            Console.WriteLine("Enter the First Number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Four Number");
+            int fourNumber = Convert.ToInt32(Console.ReadLine());
+
+            int OutPut = (firstNumber + secondNumber + thirdNumber + fourNumber) / 4;
+
+            Console.WriteLine("The average of :" + firstNumber + " , " + secondNumber + "  ,  " + thirdNumber + "  and  " + fourNumber + "  is :  " + OutPut);
+
+            #endregion
+
+            #region Homework 3
+
+            Console.WriteLine("----------- SWAP NUMBERS -----------");
+
+            int firstSwapNumber = 5;
+            Console.WriteLine("Input the First Number:" + firstSwapNumber);
+            int secondSwapNumber = 8;
+            Console.WriteLine("Input the Second Number:" + secondSwapNumber);
+            int swap;
+
+            swap = firstSwapNumber;
+            firstSwapNumber = secondSwapNumber;
+            secondSwapNumber = swap;
+
+            Console.WriteLine("***** After swapping numbers ****");
+            Console.WriteLine("First Number is :" + firstSwapNumber);
+            Console.WriteLine("Second Number is :" + secondSwapNumber);
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
